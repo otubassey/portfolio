538416232a51d468e6d8a34b30b83af81ab2d71e
Otu Bassey <>
2024-05-06 23:15:13 -0500

    updating next config

--- hello-world/next.config.mjs
+++ hello-world/next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  // output: "export",
+  distDir: "dist",
+  // useFileSystemPublicRoutes: false
 };
 
 export default nextConfig;
